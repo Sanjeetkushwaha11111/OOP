0e4ae5c971c286961768bf1e33d8b8ae5bc707f9
SanjeetKushwaha <sanjeetkushwaha1111gmailom>
2021-07-20 19:46:44 +0530

    Used Array in Classes and maked a program of small shop.

--- ObjectandClasses/ArraysInClass.cpp
+++ ObjectandClasses/ArraysInClass.cpp
@@ -1,38 +1,47 @@
-#include<bits/stdc++.h>
-using namespace std;
+	#include<bits/stdc++.h>
+	using namespace std;
 
-class shop{
-private:
+	class shop{
+	private:
 
-  int itemCounter;
-  string itemName[100];
-  int itemPrice[100];
+	int itemCounter;
+	string itemName[100];
+	int itemPrice[100];
+  int itemNumber[100];
+	public:
+	void getCounter(void) {itemCounter=0;}
+	void setItemDetails(void);
+	void displayItemDeatils(void);
+	};
 
-public:
-  void getCounter(void) {itemCounter=0;}
-  void setItemPrice(void);
-  void displayItem(void);
-};
+	void shop :: setItemDetails(void){
+	std::cout << "Enter Product name "<<":  ";
+	std::cin >> itemName[itemCounter];
 
-void shop :: setItemPrice(void){
-  std::cout << "Enter Product name "<< itemCounter+1  << ":  ";
-  std::cin >> itemName[itemCounter];
+	std::cout << "Enter Product Price: ";
+	std::cin >> itemPrice[itemCounter];
 
-  std::cout << "Enter Product Price: ";
-  std::cin >> itemPrice[itemCounter];
-  itemCounter++;
-}
+  std::cout << "Enter Number of "<<itemName[itemCounter]<<" to be added: " << '\n';
+  std::cin >> itemNumber[itemCounter];
 
-void shop :: displayItem(void){
-  for (int i = 0; i < itemCounter; i++) {
-      std::cout << "The "<<itemName[i]<<" is added and price set to: "<<itemPrice[i] << '\n';
+	itemCounter++;
+	}
+
+	void shop :: displayItemDeatils(void){
+	for (int i = 0; i < itemCounter; i++) {
+	  std::cout << "The "<<itemName[i]<<" is added and price set to: "<<itemPrice[i] << '\n';
   }
-}
+	}
 
-int main(int argc, char const *argv[]) {
-   shop sh;
-   sh.getCounter();
-   sh.setItemPrice();
-   sh.displayItem();
-  return 0;
-}
+	int main(int argc, char const *argv[]) {
+	shop sh;
+	int n;
+	std::cout << "Enter number of Product: ";
+	std::cin >> n;
+	sh.getCounter();
+	for (int i = 0; i < n; i++) {
+	   sh.setItemDetails();
+   }
+	sh.displayItemDeatils();
+	return 0;
+	}
